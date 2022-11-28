21c5774d2166313ed29cce6e05eb78d3a7a299d2
Julio Cesar Martinez Garcia <julio.martinezg8@gmail.com>
2022-11-28 15:14:30 -0500

    Evidencia 4 - ejercicio 2

--- evidencia_4/main.js
+++ evidencia_4/main.js
@@ -1,21 +1,69 @@
 window.addEventListener('load', function () {
-    /*Desarrollar un programa que permita calcular el area
-    o perimetro de algunas figuras planas
-    1. Triangulo
-    2. Rectángulo
-    3. Cuadrado
-    4. Circulo*/
-    let base, altura;
+/**Desarrollar un programa que permita almacenar las edades de un grupo de 10 personas en un vector de
+enteros y luego determine la cantidad de personas que son menores de edad, mayores de edad, cuántos
+adultos mayores, la edad más baja, la edad más alta y el promedio de edades ingresadas. Para el ejercicio
+anterior suponga que un adulto mayor debe tener una edad igual o superior a 60. Debe validar para cada
+ingreso que los valores estén en un rango entre 1 y 120 años. En caso de error deberá notificar y solicitar
+un nuevo valor. 
+ */
+
+    let edades = {}
+    let persona;
+    let edad;
+    let contador = 1;
     do {
-        base = parseInt(prompt("Ingrese base"))
-        altura = parseInt(prompt("Ingrese altura"))
+        persona = prompt(`Id: ${contador} Ingrese su nombre: `)
+        edad = parseInt(prompt("¿Cúantos años tienes?"))
+        if (isNaN(edad) || edad < 0 || edad > 120) {
+            alert("Ingrese una edad válida")
+            while (edad < 121) {
+                edad = parseInt(prompt("¿Cúantos años tienes?"))
+                edades[persona] = edad;
+                contador++
+            }
 
-    } while (base <= 0 || isNaN(base) || altura <= 0 || isNaN(altura));
+        } else {
+            contador++
+            edades[persona] = edad
+        }
 
-    function calcularAreaTriangulo(b, h) {
-        let area = (b*h)/2
-        document.write(`El area del triangulo es: ${area} U²`)
-    }
-    calcularAreaTriangulo(base, altura)
+    } while (contador <= 10);
+    let edadesKeys = Object.values(edades)
+    let menoresEdad = []
+    let mayoresEdad = []
+    let adultosMayoresEdad = []
+
+    edadesKeys.map((element) => {
+        if (element < 18) {
+            menoresEdad.push(element);
+
+        } else if (element >= 18 && element <= 59) {
+            mayoresEdad.push(element);
+        } else if (element >= 60 && element < 120) {
+            adultosMayoresEdad.push(element);
+        }
+
+
+    })
+    let suma;
+    let indice;
+    let sumaEdades = edadesKeys.reduce((element, acum, index) => {
+        suma = element += acum;
+        indice = index + 1;
+        return suma
+    })
+    let promedioEdades = suma/indice
+
+    document.write(`
+    <h1>Datos personales</h1>
+    <p>
+    N° Personas Menores de edad: ${menoresEdad.length}<br>
+    N° Personas Mayores de edad: ${mayoresEdad.length}<br>
+    N° Personas Adultos Mayores: ${adultosMayoresEdad.length}<br>
+    Edad más baja: ${Math.min(...edadesKeys)} <br>
+    Edad más alta: ${Math.max(...edadesKeys)}<br>
+    Promedio de edades : ${promedioEdades.toFixed()} años</p>
+    `)
+
 
 })
