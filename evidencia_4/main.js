286aacebadea7693b38b75aecdd01b0ff699e00c
Julio Cesar Martinez Garcia <julio.martinezg8@gmail.com>
2022-11-28 21:51:48 -0500

    Evidencia 4 - ejercicio 3

--- evidencia_4/main.js
+++ evidencia_4/main.js
@@ -1,69 +1,42 @@
 window.addEventListener('load', function () {
-/**Desarrollar un programa que permita almacenar las edades de un grupo de 10 personas en un vector de
-enteros y luego determine la cantidad de personas que son menores de edad, mayores de edad, cuántos
-adultos mayores, la edad más baja, la edad más alta y el promedio de edades ingresadas. Para el ejercicio
-anterior suponga que un adulto mayor debe tener una edad igual o superior a 60. Debe validar para cada
-ingreso que los valores estén en un rango entre 1 y 120 años. En caso de error deberá notificar y solicitar
-un nuevo valor. 
- */
+    /**. Escriba un programa que lea dos vectores de números enteros ordenados ascendentemente y luego
+    produzca la lista ordenada de la mezcla de los dos, por ejemplo, si los dos arreglos tienen los números 1
+    3 6 9 17 y 2 4 10 17, respectivamente, la lista de números en la pantalla debe ser 1 2 3 4 6 9 10 17 17.
+    Limite los vectores a un tamaño de 5 y debe validar en cada ingreso que realmente se estén ingresando
+    los datos de forma ascendente. 
+     */
 
-    let edades = {}
-    let persona;
-    let edad;
+    let vectNum1 = [];
+    let vectNum2 = [];
     let contador = 1;
-    do {
-        persona = prompt(`Id: ${contador} Ingrese su nombre: `)
-        edad = parseInt(prompt("¿Cúantos años tienes?"))
-        if (isNaN(edad) || edad < 0 || edad > 120) {
-            alert("Ingrese una edad válida")
-            while (edad < 121) {
-                edad = parseInt(prompt("¿Cúantos años tienes?"))
-                edades[persona] = edad;
+    let arreglo1;
+    let arreglo2;
+
+    function llenarArreglos() {
+        do {
+            arreglo1 = parseInt(prompt(`Array 1: Ingrese element ${contador}`));
+            arreglo2 = parseInt(prompt(`Array 2: Ingrese element ${contador}`));
+
+            if (isNaN(arreglo1) || arreglo1 < 1 || isNaN(arreglo2) || arreglo2 < 1) {
+                arreglo1 = parseInt(prompt(`Array 1: Ingrese element ${contador}`));
+                arreglo2 = parseInt(prompt(`Array 2: Ingrese element ${contador}`));
+                contador
+
+            }else{
+                vectNum1.push(arreglo1)
+                vectNum2.push(arreglo2)
                 contador++
+
             }
+        } while (contador<6);
 
-        } else {
-            contador++
-            edades[persona] = edad
-        }
+        let vectMezclado = vectNum1.concat(vectNum2);
 
-    } while (contador <= 10);
-    let edadesKeys = Object.values(edades)
-    let menoresEdad = []
-    let mayoresEdad = []
-    let adultosMayoresEdad = []
-
-    edadesKeys.map((element) => {
-        if (element < 18) {
-            menoresEdad.push(element);
-
-        } else if (element >= 18 && element <= 59) {
-            mayoresEdad.push(element);
-        } else if (element >= 60 && element < 120) {
-            adultosMayoresEdad.push(element);
-        }
+        console.log(vectMezclado.sort((a, b) => { return a - b }));
 
 
-    })
-    let suma;
-    let indice;
-    let sumaEdades = edadesKeys.reduce((element, acum, index) => {
-        suma = element += acum;
-        indice = index + 1;
-        return suma
-    })
-    let promedioEdades = suma/indice
-
-    document.write(`
-    <h1>Datos personales</h1>
-    <p>
-    N° Personas Menores de edad: ${menoresEdad.length}<br>
-    N° Personas Mayores de edad: ${mayoresEdad.length}<br>
-    N° Personas Adultos Mayores: ${adultosMayoresEdad.length}<br>
-    Edad más baja: ${Math.min(...edadesKeys)} <br>
-    Edad más alta: ${Math.max(...edadesKeys)}<br>
-    Promedio de edades : ${promedioEdades.toFixed()} años</p>
-    `)
-
+    }
+  
+    llenarArreglos()
 
 })
