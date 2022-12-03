1d52ff0c895316f7940b441c106733961db92474
juliocmartinez <95548560+juliocmartinez@users.noreply.github.com>
2022-12-03 13:44:10 -0500

    Evidencia 3 - ejercicio 3

--- evidencia_3/main.js
+++ evidencia_3/main.js
@@ -1,27 +1,37 @@
 window.addEventListener('load', function () {
     /**
-     * 2. Realizar la conversión de una temperatura dada en grados Centígrados
-     *  a grados Fahrenheit (Fórmula: F = (9/5) C + 32). 
+     *  Escribir el algoritmo que permite calcular la nota correspondiente al primer parcial de “análisis” para un 
+ estudiante cualquiera. Se debe considerar que hay dos talleres y un quiz, que en conjunto valen un 30% de 
+ la nota y el resto (70%) corresponde a la nota del examen parcial. 
      */
 
-    let boton = document.querySelector("#boton")
+    let notaTaller1, notaTaller2, notaQuiz, examenParcial, notaFinal;
 
+    function calcularNotaFinal() {
+        notaTaller1 = parseFloat(prompt("Nota taller 1: "))
+        notaTaller2 = parseFloat(prompt("Nota taller 2: "))
+        notaQuiz = parseFloat(prompt("Nota quiz: "))
+        examenParcial = parseFloat(prompt("Nota parcial: "))
 
-    boton.addEventListener('click', (temperatura) => {
-        temperatura = prompt("ingrese temperatura: ")
+        while (isNaN(notaTaller1) || notaTaller1 == null || notaTaller1 == "" || notaTaller1<=0 || notaTaller1>=5.01) {
+            notaTaller1 =parseFloat(prompt("Ingrese nota taller 1 valida: "))
 
-        if (temperatura > 0 && temperatura < Infinity) {
-            let formula = ((9 / 5) * temperatura + 32)
-            document.write(`<h1>Conversión de °C a °F</h1>
-            ${temperatura}°C equivalen a ${formula}°F`)
-            
-        }else{
-            this.document.write(`Digitos invalidos`)
+        } while (isNaN(notaTaller2) || notaTaller2 == null || notaTaller2 == "" || notaTaller2<=0 || notaTaller2>=5.01) {
+            notaTaller2 = parseFloat(prompt("Ingrese nota taller 2 valida: "))
+
+        } while (isNaN(notaQuiz) || notaQuiz == null || notaQuiz == "" || notaQuiz<=0 || notaQuiz>=5.01) {
+            notaQuiz = parseFloat(prompt("Ingrese nota quiz valida: "))
+
+        } while (isNaN(examenParcial) || examenParcial == null || examenParcial == "" || examenParcial<=0 || examenParcial>=5.01) {
+            examenParcial = parseFloat(prompt("Ingrese nota parcial valida: "))
+
         }
+        notaFinal = (notaTaller1*0.1 + notaTaller2*0.1 + notaQuiz*0.1 + examenParcial*0.7)
 
+document.write(`<h1>Nota final: ${notaFinal}</h1>`)
+    }
 
-    })
-
+    calcularNotaFinal()
 
 
 
