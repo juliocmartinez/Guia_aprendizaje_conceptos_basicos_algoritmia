8af560464f5a46ff772d71c71075de1d05676c42
Julio Cesar Martinez Garcia <julio.martinezg8@gmail.com>
2022-12-04 16:48:51 -0500

    Evidencia 3 - ejercicio 4

--- evidencia_3/main.js
+++ evidencia_3/main.js
@@ -1,37 +1,31 @@
 window.addEventListener('load', function () {
-    /**
-     *  Escribir el algoritmo que permite calcular la nota correspondiente al primer parcial de “análisis” para un 
- estudiante cualquiera. Se debe considerar que hay dos talleres y un quiz, que en conjunto valen un 30% de 
- la nota y el resto (70%) corresponde a la nota del examen parcial. 
-     */
+    /**4. Un capital C está situado a un tipo de interés R 
+     * anual 
+     * ¿al término de cuántos años se doblará?  */
 
-    let notaTaller1, notaTaller2, notaQuiz, examenParcial, notaFinal;
+    function dineroDuplicado() {
+        let capital,interesAnual
 
-    function calcularNotaFinal() {
-        notaTaller1 = parseFloat(prompt("Nota taller 1: "))
-        notaTaller2 = parseFloat(prompt("Nota taller 2: "))
-        notaQuiz = parseFloat(prompt("Nota quiz: "))
-        examenParcial = parseFloat(prompt("Nota parcial: "))
+        do {
+             capital = parseFloat(prompt("Capital de inversion"))
+             interesAnual = parseFloat(prompt("Interes anual"))
+        }
+        while (isNaN(capital) || capital == null || capital == "" || isNaN(interesAnual) || interesAnual == null || interesAnual == "")
 
-        while (isNaN(notaTaller1) || notaTaller1 == null || notaTaller1 == "" || notaTaller1<=0 || notaTaller1>=5.01) {
-            notaTaller1 =parseFloat(prompt("Ingrese nota taller 1 valida: "))
+        let capitalDuplicado = capital * 2
+        let dinero = (capital * interesAnual) / 100
+        let contadorYears = 0
 
-        } while (isNaN(notaTaller2) || notaTaller2 == null || notaTaller2 == "" || notaTaller2<=0 || notaTaller2>=5.01) {
-            notaTaller2 = parseFloat(prompt("Ingrese nota taller 2 valida: "))
-
-        } while (isNaN(notaQuiz) || notaQuiz == null || notaQuiz == "" || notaQuiz<=0 || notaQuiz>=5.01) {
-            notaQuiz = parseFloat(prompt("Ingrese nota quiz valida: "))
-
-        } while (isNaN(examenParcial) || examenParcial == null || examenParcial == "" || examenParcial<=0 || examenParcial>=5.01) {
-            examenParcial = parseFloat(prompt("Ingrese nota parcial valida: "))
+        while (capital < capitalDuplicado) {
+            capital += dinero
+            contadorYears++
 
         }
-        notaFinal = (notaTaller1*0.1 + notaTaller2*0.1 + notaQuiz*0.1 + examenParcial*0.7)
-
-document.write(`<h1>Nota final: ${notaFinal}</h1>`)
+        document.write(`<h1>En ${contadorYears} años el capital  se duplicará a ${capital} Dolares <br> Tasa efectiva anual: ${interesAnual}%</h1>`)
     }
 
-    calcularNotaFinal()
+    dineroDuplicado()
+
 
 
 
