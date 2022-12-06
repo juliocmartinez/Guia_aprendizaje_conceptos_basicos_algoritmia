1f3f40606a6d0d1050dd8a285b26e9328f82124e
juliocmartinez <95548560+juliocmartinez@users.noreply.github.com>
2022-12-06 16:50:37 -0500

    Evidencia3-ejercicio10

--- evidencia_3/main.js
+++ evidencia_3/main.js
@@ -1,20 +1,21 @@
 window.addEventListener('load', function () {
     /**
-     * 9. Dado N, escribir el producto desde 1 hasta N. 
+     * 10. Realizar un algoritmo que muestre por pantalla la tabla de multiplicar
+     *  decreciente de cualquier número, ingresado entre el 1 y el 10. 
 
      */
     let numero;
-    while (isNaN(numero) || numero == "" || numero == null) {
+    while (isNaN(numero) || numero == "" || numero == null || numero>10 || numero<=0) {
         numero = parseInt(prompt("Producto del numero: "))
-        if(!isNaN(numero)){
-            this.document.write(`<h1>Producto de ${numero}</h1>`)
+        if(!isNaN(numero) && numero!=0  && numero>=1 && numero<=10){
+            document.write(`<h1>Producto de ${numero}</h1>`)
+            for (let index = numero; index >=1; index--) {
+
+                document.write(`${numero} x ${index} = ${numero * index}<br>`);
+            }
         }
 
-        for (let index = 1; index <= numero; index++) {
-
-            this.document.write(`${numero} x ${index} = ${numero * index}<br>`);
-
-        }
+        
     }
 
 
