75ebd9722db27ce2486852ede6c946d2e2990c26
juliocmartinez <95548560+juliocmartinez@users.noreply.github.com>
2022-12-06 15:27:58 -0500

    Evidencia3-ejercicio8

--- evidencia_3/main.js
+++ evidencia_3/main.js
@@ -1,57 +1,29 @@
 window.addEventListener('load', function () {
-    /*Hacer un programa que registre el consumo realizado 
-    por los clientes de un restaurante, si el consumo de
+    /*Diseñar un algoritmo que permita ingresar la hora,
+     minutos y segundos, y que calcule la hora en el siguiente
+     segundo ("0<= H <=23", "0<= M <=59" "0<= S<=59"). */
 
-cada cliente excede 50000 se hará un descuento del 20%. Se debe mostrar el pago de cada cliente y el total
-de todos los pagos. */
+    let h = parseInt(prompt("Ingrese la hora [0-23]"))
+    let m = parseInt(prompt("Ingrese los minutos [0-59]"))
+    let s = parseInt(prompt("Ingrese los segundos [0-59]"))
 
-    let numClientes = parseInt(prompt("Clientes para registrar"))
-    let arrayClientes = []
-    let container = document.querySelector("#container")
-    let ul = document.createElement("ul")
-
-
-    for (let i = 1; i <= numClientes; i++) {
-        let clienteName = (prompt("Nombre cliente " + i))
-        let clienteConsumo = parseFloat(prompt("Ingrese consumo cliente " + i))
-        while (isNaN(clienteConsumo) || clienteConsumo == null || clienteConsumo == "") {
-            alert("Valor incorrecto, intente nuevamente")
-            clienteConsumo = parseFloat(prompt("Ingrese consumo cliente " + i))
+    if ((h >= 0 && h <= 23) && (m >= 0 && m <= 59) && (s >= 0 && s <= 59)) {
+        s += 1
+        if (s == 60) {
+            s = 0
+            m += 1
+            if (m == 60) {
+                m = 0
+                h += 1
+                if (h == 24) {
+                    h = 0
+                }
+            }
         }
-
-        arrayClientes.push({ cliente: clienteName, consumoNeto: clienteConsumo })
     }
-    arrayClientes.map((index) => {
-        // (index.consumo>=50000) ? console.log(index.consumo*=0.8) : console.log("No aplicó descuento");;
-        if (index.consumoNeto >= 50000) {
-            index.consumoDesc = index.consumoNeto * 0.2
-            index.consumoTotal = index.consumoNeto - index.consumoDesc
+    document.write(`<h2>La hora es</h2><h3>hh:${h} mm:${m} ss:${s}</h3>`)
 
-            // console.log(index);
-        } else {
-            index.consumoDesc = 0
-            index.consumoTotal = index.consumoNeto
-            // console.log(index);
 
-        }
-        let sumaTotal = 0
-        sumaTotal += index.consumoTotal
-        let li = document.createElement('li');
-        li.append(`Cliente: ${index.cliente} -- Consumo Neto: ${index.consumoNeto} -- Descuento: ${index.consumoDesc} -- Total a Pagar: ${index.consumoTotal}`)
-        ul.append(li)
-
-        container.append(ul)
-
-    })
-
-    let total = arrayClientes.reduce((acumulador, actual) =>
-        acumulador + actual.consumoTotal, 0
-
-    )
-    let suma = document.createElement('h2')
-    suma.append("Total: COP ", total)
-    container.append(ul)
-    container.append(suma)
 
 
 })
