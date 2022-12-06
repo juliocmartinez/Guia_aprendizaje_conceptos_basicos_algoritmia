7e238764e936848d1c547648ca4a9d8209dbfe28
juliocmartinez <95548560+juliocmartinez@users.noreply.github.com>
2022-12-06 16:25:41 -0500

    Evidencia3-ejercicio9

--- evidencia_3/main.js
+++ evidencia_3/main.js
@@ -1,27 +1,23 @@
 window.addEventListener('load', function () {
-    /*Diseñar un algoritmo que permita ingresar la hora,
-     minutos y segundos, y que calcule la hora en el siguiente
-     segundo ("0<= H <=23", "0<= M <=59" "0<= S<=59"). */
+    /**
+     * 9. Dado N, escribir el producto desde 1 hasta N. 
 
-    let h = parseInt(prompt("Ingrese la hora [0-23]"))
-    let m = parseInt(prompt("Ingrese los minutos [0-59]"))
-    let s = parseInt(prompt("Ingrese los segundos [0-59]"))
+     */
+    let numero;
+    while (isNaN(numero) || numero == "" || numero == null) {
+        numero = parseInt(prompt("Producto del numero: "))
+        if(!isNaN(numero)){
+            this.document.write(`<h1>Producto de ${numero}</h1>`)
+        }
 
-    if ((h >= 0 && h <= 23) && (m >= 0 && m <= 59) && (s >= 0 && s <= 59)) {
-        s += 1
-        if (s == 60) {
-            s = 0
-            m += 1
-            if (m == 60) {
-                m = 0
-                h += 1
-                if (h == 24) {
-                    h = 0
-                }
-            }
+        for (let index = 1; index <= numero; index++) {
+
+            this.document.write(`${numero} x ${index} = ${numero * index}<br>`);
+
         }
     }
-    document.write(`<h2>La hora es</h2><h3>hh:${h} mm:${m} ss:${s}</h3>`)
+
+
 
 
 
